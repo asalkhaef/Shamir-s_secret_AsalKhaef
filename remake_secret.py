c85f053eb91a81129e205069e28f01bc74eb713e
asalkhaef <asal.khaef@gmail.com>
2024-07-01 22:49:10 +0330

    add remake_secret.py and lagrange function

---
+++ remake_secret.py
@@ -0,0 +1,12 @@
+def lagrange_interpolate_at_zero(x_values, y_values, mod):
+    secret = 0
+    for i in range(len(y_values)):
+        lagrange_basis_polynomial = 1
+        for j in range(len(y_values)):
+            if i == j:
+                continue
+            multiplicative_inverse = pow(x_values[i] - x_values[j], -1, mod)
+            lagrange_basis_polynomial *= (0 - x_values[j]) * multiplicative_inverse
+        lagrange_basis_polynomial *= y_values[i]
+        secret = (secret + lagrange_basis_polynomial) % mod
+    return secret
