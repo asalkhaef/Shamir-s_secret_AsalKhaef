493d89652a4a5420155a97433e3688eebfbee8ea
asalkhaef <asal.khaef@gmail.com>
2024-07-01 22:52:56 +0330

    Create method for remake secret

--- remake_secret.py
+++ remake_secret.py
@@ -10,3 +10,9 @@ def lagrange_interpolate_at_zero(x_values, y_values, mod):
         lagrange_basis_polynomial *= y_values[i]
         secret = (secret + lagrange_basis_polynomial) % mod
     return secret
+
+
+def remake_secret(threshold, total_shares, mod, shares):
+    x_values = [x for x, y in shares]
+    y_values = [y for x, y in shares]
+    return lagrange_interpolate_at_zero(x_values, y_values, mod)
